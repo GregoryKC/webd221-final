dc6272096453ec087ca98e0fd04fa1f314fdc330
GregoryKC <n01012982@humbermail.ca>
2014-12-10 23:59:45 -0500

    Sorry for the late submission

---
+++ Christina's Project FINAL/header.php
@@ -0,0 +1,31 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+		<meta name="author" content="Greg Chao">
+		<title>Final Assignment</title>
+		<meta name="description" content="Final Project for WEBD221. Created by Gregory Chao">
+		<link href="http://netdna.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.css" rel="stylesheet">
+		<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+		<link rel="icon" href="favicon.ico" type="image/x-icon">
+		<link rel="stylesheet" href="assets/css/index.css">
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+
+	</head>
+	<body>
+		<nav class="wrapper">
+			<h1>The<strong>Website</strong></h1>
+			<span class="fa fa-bars main fa-2x" title="menu"></span>
+			<ul>
+				<li><a href="index.html">Home</a></li>
+				<li><a href="about.html">About</a></li>
+				<li><a href="portfolio.html">Portfolio</a></li>			
+			</ul>
+		</nav>
+		<header>
+			<div class="wrapper clearfix">
+				<div id="h1Wrap"><h1>Welcome to the Website</h1></div>
+				<div id="h2Wrap"><h2>It is a Nice Website</h2></div>
+			</div>
+		</header>
